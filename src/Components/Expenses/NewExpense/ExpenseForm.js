8f2c4fa23225c623304794127156b5bee3cc0745
dneog <debashisneog23@gmail.com>
2023-04-08 16:39:31 +0530

    Multiple States

--- src/Components/Expenses/NewExpense/ExpenseForm.js
+++ src/Components/Expenses/NewExpense/ExpenseForm.js
@@ -1,10 +1,21 @@
-import React from'react';
+import React, { useState } from'react';
 import './ExpenseForm.css'
 const ExpenseForm =()=>{
-    
-    const getData= (event)=>{
-        console.log(event.target.value)
+    const [enteredTitle, SetEnteredTitle]= useState('');
+    const [enteredAmount, SetEnteredAmount]= useState('');
+    const [enteredDate, SetEnteredDate]= useState('');
+
+    const titleHandler=(event)=>{
+        SetEnteredTitle(event.target.value)
     }
+  
+    const amountHandler=(event)=>{
+        SetEnteredAmount(event.target.value)
+    }
+    const dateHandler=(event)=>{
+        SetEnteredDate(event.target.value)
+    }
+  
       
        
    
@@ -14,19 +25,19 @@ const ExpenseForm =()=>{
     <div className='mainForm'>
     <div className='expense-form1'>
     <label className='label'>Title</label>
-    <input type="text" onChange={getData} />
+    <input type="text" onChange={titleHandler} />
 
     </div>
     <div className='expense-form1'>
     <label className='label'>Amount</label>
-    <input type="number" />
+    <input type="number" onChange={amountHandler} />
 
     </div>
    
     
     <div className='expense-form1'>
     <label className='label'>Date</label>
-    <input type="date" min='2020-01-01' max='2023-03-01'/>
+    <input type="date" min='2020-01-01' max='2023-03-01' onChange={dateHandler}/>
 
     </div>
     </div>
